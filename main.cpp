a5283607d5ad257b02f8e582199f7ef23bd24e39
Nikita Krenov <nikitabaram@gmail.com>
2024-04-20 17:43:48 +0300

    new

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double L;
+    double pi = 3.14;
+
+    cout << "Enter the length of the circle: ";
+    double R = L / (2 * pi);
+    double S = pi * R * R;
+    cout << "The radius of the circle: " << R << endl;
+    cout << "Circle area: " << S << endl;
+
+    system("pause");
+    return 0;
+}
