70f225b5a64dcc03319c08afe12d86c4234c6d58
Nikita Krenov <nikitabaram@gmail.com>
2024-04-20 17:33:35 +0300

    Update

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int R1 = 2;
+    int R2 = 4;
+    int R3 = 8;
+    double R0;
+
+    R0 = 1.0 / (1.0 / R1 + 1.0 / R2 + 1.0 / R3);
+
+    cout << "Значення опору R0: " << R0 << endl;
+
+    system("pause");
+    return 0;
+}
